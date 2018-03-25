df177574e4d32c6d41ac1c8f18e74469867b02dd
Wes <a>
2018-03-25 09:53:09 -0400

    finishing touches

--- index.js
+++ index.js
@@ -37,28 +37,51 @@ var params = {
  */
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
+    
+    var payload = [];
+    var operation;
+    if (event.body !== null && event.body !== undefined) {
+        let body = JSON.parse(event.body)
+        if (body.payload) {
+            payload = body.payload;
+        } 
+        if (body.operation) {
+            operation = body.operation;
+        }
+        
+        
+    }
+    
+    
+    
     var responseBody = {
     };
-	
-    const operation = event.TYPE;
-    var payload = {};
     
     switch (operation) {
         
         case 'insert':
-            payload.TableName = "Feedback";
+            var params = []
+            params.TableName = "Feedback";
             var d = new Date();
             var item = {
-              "MESSAGE_ID": 4321,
-              "USER_ID": event.USER_ID,
-              "EMOJI": event.EMOJI
+              "MESSAGE_ID": payload.MESSAGE_ID,
+              "USER_ID": payload.USER_ID,
+              "EMOJI": payload.EMOJI,
+              "TS_CREATED": String(d)
             }
-			payload.Item = item;
-            dynamo.putItem(payload, null);
+			params.Item = item;
+			
+            dynamo.putItem(params, function(err, data) {
+            if (err) {
+                console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Added item:", JSON.stringify(data, null, 2));
+            }});
             break;
         case 'list':
             payload.TableName = "Feedback";
             dynamo.scan(payload, callback);
+            return;
             break;
         case 'echo':
             responseBody.response = payload;
